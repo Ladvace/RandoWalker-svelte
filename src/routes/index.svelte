9b9c6d67a535fec8a92651c8cf2d714530c95ddd
Gianmarco <cavallogianmarco@gmail.com>
2021-03-28 19:29:59 +0200

    sounds effect and animations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,215 +1,178 @@
 <script>
 	import { onMount } from 'svelte';
-	let GRID_SIZE = 30;
-	let TICKER_TIME = 200;
+	import sound from '../../static/sound.wav';
+	let GRID_SIZE = 40;
+	let TICKER_TIME = 500;
 	let grid = [...Array(GRID_SIZE)].map(() => [...Array(GRID_SIZE)].map(() => 'empty'));
-	let finished = false;
 	let walkerPosition = [[GRID_SIZE / 2, GRID_SIZE / 2]];
+	let walls = [];
 	let isMouseDown = false;
+	let interval;
+	let paused = false;
+	let visitedCellCounter = 0;
+	let wallsCounter = 0;
+	let musicEnabled = false;
 
-	// function randomFood() {
-	// 	grid[getRandomInt(GRID_SIZE)][getRandomInt(GRID_SIZE)] = 'visited';
-	// 	grid[getRandomInt(GRID_SIZE)][getRandomInt(GRID_SIZE)] = 'visited';
-	// }
+	const playAudio = () => {
+		let audio = document.getElementById('audio');
+
+		audio.play();
+	};
 
 	$: {
-		// console.log('refresh', walkerPosition);
-
-		for (let i = 0; i < grid.length; i++) {
-			for (let k = 0; k < grid.length; k++) {
-				if (grid[i][k] === 'visited') {
-					grid[i][k] = 'empty';
+		// console.log('TICKER_TIME', TICKER_TIME);
+		if (!paused) {
+			for (let i = 0; i < grid.length; i++) {
+				for (let k = 0; k < grid.length; k++) {
+					if (grid[i][k] === 'visited' || grid[i][k] === 'revisited') {
+						grid[i][k] = 'empty';
+					}
 				}
 			}
+
+			walkerPosition.forEach(([x, y]) => {
+				if (grid[x][y] === 'wall') return;
+
+				if (grid[x][y] === 'visited' || grid[x][y] === 'revisited') {
+					grid[x][y] = 'revisited';
+				} else grid[x][y] = 'visited';
+			});
 		}
-
-		walkerPosition.forEach(([x, y]) => {
-			// console.log('ref', x, y);
-			if (grid[x][y] === 'wall') return;
-
-			if (grid[x][y] === 'visited' || grid[x][y] === 'revisited') {
-				grid[x][y] = 'revisited';
-			} else grid[x][y] = 'visited';
-		});
 	}
 
 	function getRandomInt(max) {
 		return Math.floor(Math.random() * Math.floor(max));
 	}
 
-	// class walker {
-	// 	constructor() {
-	// 		this.x = GRID_SIZE / 2;
-	// 		this.y = GRID_SIZE / 2;
-	// 		this.color = '#fff';
-	// 	}
-	// 	display() {
-	// 		this.color = '#0000';
-	// 	}
-
-	// 	step() {
-	// 		const randomX = getRandomInt(4);
-	// 		const randomY = getRandomInt(4);
-
-	// 		const lastVisited = walkerPosition[walkerPosition.length - 1];
-
-	// 		if (choice == 0) {
-	// 			// this.x++;
-	// 			walkerPosition = walkerPosition.concat(lastVisited[0] + 1, lastVisited[1]);
-	// 		} else if (choice == 1) {
-	// 			// this.x--;
-	// 			walkerPosition = walkerPosition.concat(lastVisited[0] - 1, lastVisited[1]);
-	// 		} else if (choice == 2) {
-	// 			// this.y++;
-	// 			walkerPosition = walkerPosition.concat(lastVisited[0], lastVisited[1] + 1);
-	// 		} else {
-	// 			// this.y--;
-	// 			walkerPosition = walkerPosition.concat(lastVisited[0], lastVisited[1] - 1);
-	// 		}
-	// 	}
-	// }
-
-	const step = (time) => {
-		setTimeout(() => {
-			const randomX = getRandomInt(4) - 1;
-			const randomY = getRandomInt(4) - 1;
-
-			const lastVisited = walkerPosition[walkerPosition.length - 1];
-
-			walkerPosition = walkerPosition.concat([lastVisited[0] + randomX, lastVisited[1] + randomY]);
-
-			console.log(
-				't',
-				randomX,
-				randomY,
-				lastVisited,
-				lastVisited[0] + randomX,
-				lastVisited[1] + randomY,
-				walkerPosition
-			);
-			// if (choice == 0) {
-			// 	// this.x++;
-			// 	walkerPosition = walkerPosition.concat(lastVisited[0] + 1, lastVisited[1]);
-			// } else if (choice == 1) {
-			// 	// this.x--;
-			// 	walkerPosition = walkerPosition.concat(lastVisited[0] - 1, lastVisited[1]);
-			// } else if (choice == 2) {
-			// 	// this.y++;
-			// 	walkerPosition = walkerPosition.concat(lastVisited[0], lastVisited[1] + 1);
-			// } else {
-			// 	// this.y--;
-			// 	walkerPosition = walkerPosition.concat(lastVisited[0], lastVisited[1] - 1);
-			// }
-			step(TICKER_TIME - Math.min(walkerPosition.length, 15) * 10);
-		}, time);
-	};
-
 	function isOutOfBounds(n) {
 		return n < 0 || n > GRID_SIZE - 1;
 	}
 
 	onMount(() => {
-		// step(TICKER_TIME);
-		let interval;
 		interval = setInterval(() => {
-			// randomFood();
-			// walkerPosition[0][1] -= 1;
+			if (!paused) {
+				const lastVisited = walkerPosition[walkerPosition.length - 1];
 
-			// const randomX = getRandomInt(4) - 1;
-			// const randomY = getRandomInt(4) - 1;
-
-			const lastVisited = walkerPosition[walkerPosition.length - 1];
-
-			// const newPosY = lastVisited[1] + randomY;
-			// const newPosX = lastVisited[0] + randomX;
-
-			// console.log('finished', newPosX, newPosY, isOutOfBounds(newPosY), isOutOfBounds(newPosX));
-			const choice = getRandomInt(4);
-			// if (!isOutOfBounds(newPosY) || !isOutOfBounds(newPosX)) {
-			// console.log('rend', newPosX, newPosY, walkerPosition);
-			// walkerPosition = [...walkerPosition, [newPosX, newPosY]];
-
-			if (choice == 0) {
-				const newX = lastVisited[0] + 1;
-				const Y = lastVisited[1];
-
-				if (isOutOfBounds(newX)) return;
-				if (grid[newX][Y] === 'wall') return;
-				if (grid[newX][Y] === 'visited') {
-					console.log('revisited1', newX, Y);
-					grid[newX][Y] = 'revisited';
+				const choice = getRandomInt(4);
+				if (musicEnabled) {
+					playAudio();
 				}
+				if (choice == 0) {
+					const newX = lastVisited[0] + 1;
+					const Y = lastVisited[1];
 
-				walkerPosition = [...walkerPosition, [newX, Y]];
-			} else if (choice == 1) {
-				const newX = lastVisited[0] - 1;
-				const Y = lastVisited[1];
+					if (isOutOfBounds(newX)) return;
+					if (grid[newX][Y] === 'wall') return;
+					if (grid[newX][Y] === 'visited') {
+						grid[newX][Y] = 'revisited';
+					}
 
-				if (isOutOfBounds(newX)) return;
-				if (grid[newX][Y] === 'wall') return;
-				if (grid[newX][Y] === 'visited') {
-					console.log('revisited2', newX, Y);
-					grid[newX][Y] = 'revisited';
+					if (grid[newX][Y] === 'empty') visitedCellCounter++;
+					walkerPosition = [...walkerPosition, [newX, Y]];
+				} else if (choice == 1) {
+					const newX = lastVisited[0] - 1;
+					const Y = lastVisited[1];
+
+					if (isOutOfBounds(newX)) return;
+					if (grid[newX][Y] === 'wall') return;
+					if (grid[newX][Y] === 'visited') {
+						grid[newX][Y] = 'revisited';
+					}
+
+					if (grid[newX][Y] === 'empty') visitedCellCounter++;
+					walkerPosition = [...walkerPosition, [newX, Y]];
+				} else if (choice == 2) {
+					const X = lastVisited[0];
+					const newY = lastVisited[1] + 1;
+
+					if (isOutOfBounds(newY)) return;
+					if (grid[X][newY] === 'wall') return;
+					if (grid[X][newY] === 'visited') {
+						grid[X][newY] = 'revisited';
+					}
+
+					if (grid[X][newY] === 'empty') visitedCellCounter++;
+					walkerPosition = [...walkerPosition, [X, newY]];
+				} else {
+					const X = lastVisited[0];
+					const newY = lastVisited[1] - 1;
+
+					if (isOutOfBounds(newY)) return;
+					if (grid[X][newY] === 'wall') return;
+					if (grid[X][newY] === 'visited') {
+						grid[X][newY] = 'revisited';
+					}
+					if (grid[X][newY] === 'empty') visitedCellCounter++;
+					walkerPosition = [...walkerPosition, [X, newY]];
 				}
+				// } else {
+				// finished = true;
+				// clearInterval(interval);
+				// return;
+				// }
 
-				walkerPosition = [...walkerPosition, [newX, Y]];
-			} else if (choice == 2) {
-				const X = lastVisited[0];
-				const newY = lastVisited[1] + 1;
-
-				if (isOutOfBounds(newY)) return;
-				if (grid[X][newY] === 'wall') return;
-				if (grid[X][newY] === 'visited') {
-					console.log('revisited4', X, newY);
-					grid[X][newY] = 'revisited';
-				}
-
-				walkerPosition = [...walkerPosition, [X, newY]];
-			} else {
-				const X = lastVisited[0];
-				const newY = lastVisited[1] - 1;
-
-				if (isOutOfBounds(newY)) return;
-				if (grid[X][newY] === 'wall') return;
-				if (grid[X][newY] === 'visited') {
-					console.log('revisited5', X, newY);
-					grid[X][newY] = 'revisited';
-				}
-
-				walkerPosition = [...walkerPosition, [X, newY]];
+				// walkerPosition = walkerPosition.concat([lastVisited[0] + randomX, lastVisited[1] + randomY]);
 			}
-			// } else {
-			// finished = true;
-			// clearInterval(interval);
-			// return;
-			// }
-
-			// walkerPosition = walkerPosition.concat([lastVisited[0] + randomX, lastVisited[1] + randomY]);
-		}, 1000);
+		}, TICKER_TIME);
 	});
 </script>
 
 <main>
-	{#if finished}
-		<h1>LOST</h1>
-	{/if}
+	<h1>Random Walker</h1>
+
+	<div class="header">
+		<button on:click={() => clearInterval(interval)}>Stop</button>
+		<button on:click={() => (paused = !paused)}>{!paused ? 'Pause' : 'Resume'}</button>
+		<button
+			on:click={() => {
+				walkerPosition = [[GRID_SIZE / 2, GRID_SIZE / 2]];
+				grid = [...Array(GRID_SIZE)].map(() => [...Array(GRID_SIZE)].map(() => 'empty'));
+			}}>Clear</button
+		>
+		<label>
+			<input type="checkbox" bind:checked={musicEnabled} />
+			Sounds Effects
+		</label>
+		<!-- <p>Speed:</p>
+		<input type="number" bind:value={TICKER_TIME} /> -->
+	</div>
+
+	<div class="scoreBoard">
+		<div>Walls: {wallsCounter}</div>
+		<div>visited Cells: {walkerPosition.length}</div>
+		<div>Unique visited Cells: {visitedCellCounter}</div>
+	</div>
 	<div class="center">
 		<div>
 			<!-- on:click={() => (grid[i][k] = grid[i][k] === 'wall' ? 'empty' : 'wall')} -->
 			<!-- on:mousedown={() => (grid[i][k] = grid[i][k] === 'wall' ? 'empty' : 'wall')} -->
 			{#each grid as row, i}
 				<div class="row" on:contextmenu={(e) => e.preventDefault()}>
+					<audio id="audio" src={sound} />
 					{#each row as cell, k}
 						<div
-							on:click={() => (grid[i][k] = grid[i][k] === 'wall' ? 'empty' : 'wall')}
+							on:click={() => {
+								grid[i][k] = grid[i][k] === 'wall' ? 'empty' : 'wall';
+
+								//update wall counter when adding and removing
+								if (grid[i][k] === 'wall') {
+									walls = [...walls, grid[i][k]];
+									wallsCounter++;
+								} else wallsCounter--;
+							}}
 							on:mousedown={(e) => {
-								console.log(e);
 								isMouseDown = true;
 							}}
 							on:mouseup={() => (isMouseDown = false)}
 							on:mouseover={() => {
 								if (isMouseDown) {
 									grid[i][k] = grid[i][k] === 'wall' ? 'empty' : 'wall';
+
+									//update wall counter when adding and removing
+									if (grid[i][k] === 'wall') {
+										walls = [...walls, grid[i][k]];
+										wallsCounter++;
+									} else wallsCounter--;
 								}
 							}}
 							class={`square ${cell}`}
@@ -232,11 +195,15 @@
 	}
 
 	main {
-		height: 100vh;
+		height: auto;
 		box-sizing: border-box;
 		text-align: center;
 		padding: 0;
 		/* margin: 0 auto; */
+	}
+
+	.header {
+		margin: 10px;
 	}
 
 	.center {
@@ -253,9 +220,11 @@
 	}
 
 	.square {
-		border: 1px solid rgba(201, 201, 201, 0.836);
-		width: 30px;
-		height: 30px;
+		border: 1px solid rgba(0, 0, 0, 0.2);
+
+		stroke-opacity: 0.2;
+		width: 20px;
+		height: 20px;
 		box-sizing: border-box;
 	}
 
@@ -268,7 +237,19 @@
 	}
 
 	.revisited {
-		background-color: orange;
+		background-color: rgb(99, 99, 99);
+	}
+
+	.scoreBoard {
+		border: 2px dashed black;
+		border-top: none;
+		border-right: none;
+		border-radius: 5px;
+		background: white;
+		position: absolute;
+		top: 0;
+		right: 0;
+		padding: 50px;
 	}
 
 	@keyframes slidein {
@@ -281,17 +262,18 @@
 	}
 
 	.wall {
-		background-color: black;
+		background-color: rgb(48, 48, 48);
 		animation: slidein 0.2s ease-in forwards;
 	}
 
 	h1 {
 		color: #ff3e00;
+		font-family: monospace;
 		text-transform: uppercase;
 		font-size: 4rem;
 		font-weight: 100;
 		line-height: 1.1;
-		margin: 4rem auto;
+		margin: 0;
 		max-width: 14rem;
 	}
 
